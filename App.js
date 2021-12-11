cdbccd529bbe0f96e22dba7fb1912bc84c35226a
adujosep <josephadu@starksltd.com>
2021-12-11 13:12:07 +0100

    install packages

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as backend from "./build/index.main.mjs";
 import * as reach from "@reach-sh/stdlib/ALGO";
+import Button from "react-bootstrap/Button";
 
 const { standardUnit } = reach;
 
@@ -8,17 +9,18 @@ const App = () => {
   const [isDeployer, setIsDeployer] = useState(false);
 
   useEffect(() => {
-    async function fetchData() {
-      const acc = await reach.getDefaultAccount();
-      const balAtomic = await reach.balanceOf(acc);
-      const bal = reach.formatCurrency(balAtomic, 4);
-    }
-    fetchData();
+    // async function fetchData() {
+    //   const acc = await reach.getDefaultAccount();
+    //   const balAtomic = await reach.balanceOf(acc);
+    //   const bal = reach.formatCurrency(balAtomic, 4);
+    // }
+    // fetchData();
   }, []);
 
   return (
     <div>
       <h1>Sponsor Me Header Goes Here...</h1>
+      <Button variant="success">Button #3</Button>
     </div>
   );
 };
